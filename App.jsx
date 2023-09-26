647c8a78e4f39122fe60065fe5d933b87d4c253f
balaji N <75234946+Balajibal@users.noreply.github.com>
2023-09-26 15:22:40 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+// App.js
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import AboutMe from './components/AboutMe';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <AboutMe />
+      <Portfolio />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
---
+++ Navbar.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav>
+      {/* App.jsx */}
+    </nav>
+  );
+}
+
+export default Navbar;
